90349fe32dd654fd2d81a1523bc8a5fcc9a33ab0
waelgyousef <76689586+waelgyousef@users.noreply.github.com>
2021-01-13 19:53:58 -0500

    Add files via upload

---
+++ src/app/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{ path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
